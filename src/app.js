20ae2e3f94d6a986bc3d8ddcea850e6ac563d35a
iavila87 <ivan_avila_pr@hotmail.com>
2023-08-24 12:36:35 -0300

    [feat] init handlebars and add views.router.js

--- src/app.js
+++ src/app.js
@@ -1,13 +1,20 @@
 import express from 'express'
+import handlebars from 'express-handlebars'
+import viewRouter from './routers/views.router.js'
 import productRouter from './routers/products.router.js'
 import cartRouter from './routers/carts.router.js'
 
 /** Inicializacion de express */
 const app = express();
+/** Inicializacion de handlebars*/
+app.engine('handlebars', handlebars.engine());  // instancia handlebars
+app.set('views', './src/views');                // indica donde se encontraran las vistas 
+app.set('view engine', 'handlebars');          // confirma que el motor de plantillas es handlebars
 /** */
 app.use(express.json());
 
 /* Routers */
+app.use('/', viewRouter);
 app.use('/api/products', productRouter);
 app.use('/api/carts', cartRouter);
 
---
+++ src/routers/views.router.js
@@ -0,0 +1,26 @@
+import { Router } from "express";
+import ProductManager from '../ProductManager.js'
+
+/** Inicializacion de ProductManager */
+const pm = new ProductManager('./data/products.json');
+
+const router = Router();
+
+router.get('/', async (req, res) => {
+    // consulta productos
+    const products = await pm.getProducts();
+    let emptyProducts = false;
+    if(typeof products == 'string' || products.length == 0) {
+        emptyProducts = true;
+    }
+
+    res.render('home', { // como segundo argumento le paso argumentos como objetos
+        user: 'ivan avila',
+        emptyProducts,
+        products: products
+    });     // renderiza el home.handlebars en main.handlebars
+});
+
+
+
+export default router
