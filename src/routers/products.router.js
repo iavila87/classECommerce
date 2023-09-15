7acae2f5db4c1da54defd4a0e817337845705976
iavila87 <ivan_avila_pr@hotmail.com>
2023-09-15 12:43:03 -0300

    [fix] products.router

--- src/routers/products.router.js
+++ src/routers/products.router.js
@@ -11,41 +11,45 @@ const router = Router();
 /** get 'api/products' y 'api/products?limit='*/
 router.get('/', async (req, res) =>{
 
-    const limit = req.query.limit;
+    const limit = parseInt(req.query.limit);
     /** Acceso por archivo
         const products = await pm.getProducts();
     */
     /** Acceso por Mongoose */
     try{
-        const products = await productsModel.find();
-
-        if(typeof products == 'string') {
-            return res.status(404).send( { status: "error", error: products.split(' ').slice(2).join(' ') } );
-        }
-    
-        if(!limit) {
+        if(limit) {
+            const products = await productsModel.find().limit(limit);
             return res.status(200).send( {status: "success", payload: products } );
         }
-    
-        res.status(200).send( {status: "success", payload: products.slice(0,limit) } );
+
+        const products = await productsModel.find();
+        /* 
+        if(typeof products == 'string') {
+            return res.status(404).send( { status: "error", error: products.split(' ').slice(2).join(' ') } );
+        }*/
+        res.status(200).send( {status: "success", payload: products } );
     }
     catch(error){
         console.log("error de mongoose: "+error);
     }
-    
 });
 
 /** get 'api/products/:pid' */
 router.get('/:pid', async (req, res) =>{
 
-    const id = parseInt(req.params.pid);
-    const product = await pm.getProductById(id);
-
-    if(typeof product !== 'string'){
+    const id = req.params.pid;
+    /** por filesystem */
+    //const product = await pm.getProductById(id);
+    try{
+        /** por mongoose */
+        const product = await productsModel.find({_id:id});
+        /*if(typeof product == 'string'){
+            res.status(404).send( { status: "error", error: product.split(' ').slice(2).join(' ') } );
+        }*/
         return res.status(200).send( { status: "success", payload: product } );
+    }catch(error){
+        console.log("error: "+ error )
     }
-
-    res.status(404).send( { status: "error", error: product.split(' ').slice(2).join(' ') } );
 });
 
 /** Metodo POST */
@@ -53,43 +57,60 @@ router.post('/', async (req, res) => {
     /** por archivo */
     //const newProduct = await pm.addProduct(req.body);
     /** por mongoose */
-    const newProduct = await productsModel.create(req.body);
-
-    if(typeof newProduct == 'string'){
-        return res.status(404).send( { status: "error", error: newProduct.split(' ').slice(2).join(' ') } );
+    try{
+        const newProduct = req.body;
+        newProduct.status = true;
+        const generatedProduct = new productsModel(newProduct);
+        await generatedProduct.save();
+        // res.redirect('/'); redirecciona a la vista raiz
+        /*if(typeof newProduct == 'string'){
+            return res.status(404).send( { status: "error", error: newProduct.split(' ').slice(2).join(' ') } );
+        }*/
+        res.status(201).send( { status: "success", payload: generatedProduct } );
+    }catch(error){
+        console.log("error: " + error);
     }
-    newProduct.status = true;
-    res.status(201).send( { status: "success", payload: newProduct } );
 });
 
 /** Metodo PUT */
 router.put('/:pid', async (req, res) =>{
 
-    const id = parseInt(req.params.pid);
+    const id = req.params.pid;
+    const updateProduct = req.body;
     /** por archivo */
     //const product = await pm.updateProduct(id, req.body);
     /** por mongoose */
-    const product = await productsModel.updateOne({_id:req.params.pid}, req.body);
+    try{
+        const product = await productsModel.updateOne({_id:id}, updateProduct);
+        /*
+        if(typeof product == 'string'){
+            return res.status(404).send( { status: "error", error: product.split(' ').slice(2).join(' ') } );
+        }*/
+        res.status(200).send( { status: "success", payload: product } );
 
-    if(typeof product == 'string'){
-        return res.status(404).send( { status: "error", error: product.split(' ').slice(2).join(' ') } );
+    }catch(error){
+        console.log("error: "+error);
     }
-    res.status(200).send( { status: "success", payload: product } );
 });
 
 /** Metodo DELETE */
 router.delete('/:pid', async (req, res) =>{
 
-    const id = parseInt(req.params.pid);
+    const id = req.params.pid;
     /** por archivo */
     //const products = await pm.deleteProduct(id);
     /** por mongoose */
-    const products = await productsModel.deleteOne({_id:req.params.pid});
-
-    if(typeof products == 'string'){
-        return res.status(404).send( { status: "error", error: products.split(' ').slice(2).join(' ') } );
+    try{
+        const deleteProduct = await productsModel.deleteOne({_id:id});
+        const products = productsModel.find();
+        /*
+        if(typeof products == 'string'){
+            return res.status(404).send( { status: "error", error: products.split(' ').slice(2).join(' ') } );
+        }*/
+        res.status(200).send( { status: "success", payload: products } );
+    }catch(error){
+        console.log("error: "+error);
     }
-    res.status(200).send( { status: "success", payload: products } );
 });
 
 export default router;
